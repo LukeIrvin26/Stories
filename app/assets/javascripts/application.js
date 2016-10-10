a98a60e8dc3660f9b5e6b97f1d5eb6b8aead2ad4
codecademy <ccuser@codecademy.com>
2016-10-10 13:47:23 -0400

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+var main = function() {
+    $(".icon-menu").click(function() {
+        $(".menu").animate({
+            left: "0px"
+        }, 200);
+    });
+    $(".icon-close").click(function() {
+        $(".menu").animate({
+            left: "-240px"
+        }, 200);
+    });
+};
+
+$(document).ready(main);
